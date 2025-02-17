5c630a17096bfa9c4d584cfc30d485a1c60d1a65
paulchworks <paulchworks@gmail.com>
2025-02-17 15:07:11 +0000

    Edited UI elements

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,7 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "PaulchWorks EA"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "How can I help you?"
